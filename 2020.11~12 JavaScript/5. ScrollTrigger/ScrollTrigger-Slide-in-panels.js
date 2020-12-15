f6d2fa71389a3ed5cf0503364296fcdd5014ee85
JungminDo <k.dragonic@gmail.com>
2020-12-15 16:31:14 +0900

    ScrollTrigger study2
    Trigger study2 pin spacing slide

---
+++ 2020.11~12 JavaScript/5. ScrollTrigger/ScrollTrigger-Slide-in-panels.js
@@ -0,0 +1,48 @@
+
+
+    $(document).ready(function(){
+
+
+        // slide-in panels
+
+    //     gsap.registerPlugin(ScrollTrigger);
+    //     gsap.defaults({ ease: "none", duration: 2 });
+
+    //     // create a sequence that moves 3 of the panels in from different directions
+    //     const tle = gsap.timeline();
+    //     tle
+    //         .from(".a", { xPercent: -100 })
+    //         .from(".b", { xPercent: 100 })
+    //         .from(".c", { yPercent: -100 });
+
+    //    // pin the container and link the animation to the scrollbar (scrub: true). We could easily embed this in the gsap.timeline() to shorten things a bit, but this is to show that you can create the ScrollTrigger separately if you prefer.
+    //     ScrollTrigger.create({
+    //         animation: tle,
+    //         trigger: ".a",
+    //         start: "top top",
+    //         end: "+=4000",
+    //         scrub: true,
+    //         pin: true,
+    //         anticipatePin: 1
+    //     });
+
+
+        // gsap.registerPlugin(ScrollTrigger);
+
+        // ScrollTrigger.create({
+        //     trigger: ".b",
+        //     start: "top top",
+        //     end: "+=300px",
+        //     pin: true,
+        //     markers: true
+        // });
+
+
+        // gsap.registerPlugin(ScrollTrigger);
+
+        // let sections = gsap.utils.toArray(".main");
+
+
+
+
+    });
--- 2020.11~12 JavaScript/5. ScrollTrigger/ScrollTrigger.js
+++ 2020.11~12 JavaScript/5. ScrollTrigger/ScrollTrigger.js
@@ -48,20 +48,89 @@
 
         });
 
+        $(document).ready(function(){
 
         /* 내가 만드는 ScrollTrigger */
         gsap.registerPlugin(ScrollTrigger);
 
-        gsap.to(".orange", {
-            ScrollTrigger: {
-                trigger: ".orange",
-                start: "center center",
-                end: () => "+=" +
-        document.querySelector(".orange").offsetWidth,
-            markers: true,
-            toggleActions: "restart pause reverse pause"
-        },
+        gsap.to(".c", {
+            scrollTrigger: {
+                trigger: ".c",
+                start: "top 100px", // 스크롤시 애니메이션 시작 지점
+                endTrigger: ".c",
+                end: "bottom 20px",  // 스크롤시 애니메이션 끝나는 지점
+                // markers: true,
+                pin: true,
+                toggleActions: "restart pause reverse pause"
+            },
             x: 400,
             rotation: 360,
             duration: 3
         });
+
+        // scrub animation
+
+        gsap.to(".test2", {
+            scrollTrigger: {
+                trigger: ".test2",
+                start: "top center", // 스크롤시 애니메이션 시작 지점
+                end: "top 100px",  // 스크롤시 애니메이션 끝나는 지점
+                scrub: 3,
+                pin: ".ghost",  // ScrollTrigger 가 활성 상태 인 동안 고정되어야 하는요소
+                                // 나머지 콘텐츠가 아래에서 계속 스크롤되는 동안 시작 위치에 고정 된 것처럼 보인다.
+                                //trigger 요소가 고정되며 고정된 요소 자체에 애니메이션을 적용하지 마라
+                onEnter: () => console.log("enter"),
+                onLeave: () => console.log("leave"),
+                onEnterBack: () => console.log("enter back"),
+                onLeaveBack: () => console.log("leave back all the"),
+                markers: true,
+            },
+            x: 400,
+            rotation: 360,
+            ease: "none",
+            duration: 3
+        });
+
+        gsap.to(".ghost", {
+            scrollTrigger: {
+                trigger: ".ghost",
+                start: "top 50%", // 스크롤시 애니메이션 시작 지점
+                end: "top 10%",  // 스크롤시 애니메이션 끝나는 지점
+                scrub: true,
+                markers: true,
+            },
+            x: 400,
+            rotation: 360,
+            ease: "none",
+            duration: 3
+        });
+
+        // scrub anumation2
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".test33",
+                start: "top center", // 스크롤시 애니메이션 시작 지점
+                end: "top 100px",  // 스크롤시 애니메이션 끝나는 지점
+                scrub: 3,
+                // markers: true,
+            }
+        });
+
+        tl.to(".test33",{
+            x: 400,
+            rotation: 360,
+            ease: "none",
+            duration: 3
+        })
+        .to(".test33", {
+            backgroundColor: "purple",
+            duration: 1
+        })
+        .to (".test33", {
+            x: 0,
+            duration: 3
+        });
+
+
+    });
